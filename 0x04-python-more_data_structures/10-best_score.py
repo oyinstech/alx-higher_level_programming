a51cc44c9f31173b703baaa0329a2e7b034237a1
Oyinloye Kayode <oyinsbiz@gmail.com>
2022-09-30 14:58:10 -0700

    More Data Structure

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 
 def best_score(a_dictionary):
-    if not a_dictionary:
+    if a_dictionary:
+        for key, value in a_dictionary.items():
+            if value is max(a_dictionary.values()):
+                return 
+    else:
         return None
-    return max(a_dictionary)
