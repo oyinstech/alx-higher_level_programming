7419adaae137b4edcbe362a66015247303537991
Oyinloye Kayode <oyinsbiz@gmail.com>
2022-10-10 13:41:16 -0700

    Python Exceptions

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+def safe_print_list(my_list=[], x=0):
+    i = 0
+    while True:
+         try:
+             if i < x:
+                 print(my_list[i], end='')
+                 i += 1
+             else:
+                 print()
+                 return i
+         except IndexError:
+             print()
+             return i
